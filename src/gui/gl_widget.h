93861cd6dfb7352a3ee6f7e219ed3adfac78dc65
Xing He <xingh.dll@gmail.com>
2017-02-24 13:10:14 +0800

    1. Added more assets: shaders and textures; 2. Use a more clever CMakeLists hierarchy; 3. More complex OpenGL scene and user interactioin.

--- src/gui/gl_widget.h
+++ src/gui/gl_widget.h
@@ -1,17 +1,49 @@
 #ifndef GL_WIDGET_H_
 #define GL_WIDGET_H_
 
+#include <QtGui>
 #include <QGLWidget>
 
-class GLWidget : public QGLWidget {
-Q_OBJECT
+class QGLShaderProgram;
+
+class GLWidget : public QGLWidget
+{
+    Q_OBJECT
 
 public:
-    explicit GLWidget(QWidget *parent = 0);
+    GLWidget(QWidget *parent = 0, QGLWidget *shareWidget = 0);
+    ~GLWidget();
 
-    void resizeGL();
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+    void rotateBy(int xAngle, int yAngle, int zAngle);
+    void setClearColor(const QColor &color);
+
+signals:
+    void clicked();
+
+protected:
     void initializeGL();
     void paintGL();
+    void resizeGL(int width, int height);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+private:
+    void makeObject();
+
+    QColor clearColor;
+    QPoint lastPos;
+    int xRot;
+    int yRot;
+    int zRot;
+    GLuint textures[6];
+    QVector<QVector3D> vertices;
+    QVector<QVector2D> texCoords;
+#ifdef QT_OPENGL_ES_2
+    QGLShaderProgram *program;
+#endif
 };
 
 #endif  // GL_WIDGET_H_
